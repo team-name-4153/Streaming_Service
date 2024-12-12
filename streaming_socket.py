0f8e49e7938525394aeb6969a855f72f225bf47a
luigi617 <luigiliu617@gmail.com>
2024-12-12 05:30:01 -0500

    f

--- streaming_socket.py
+++ streaming_socket.py
@@ -188,13 +188,17 @@ class StreamingSocket(Namespace):
             if len(data) > 0:
                 ffmpeg_process.stdin.write(data)
                 ffmpeg_process.stdin.flush()
+                ffmpeg_process.stdin.close()
+
                 stdout, stderr = ffmpeg_process.communicate()
 
                 # Decode and print the output (stdout and stderr)
                 if stdout:
-                    print(f"FFmpeg stdout: {stdout.decode()}")
+                    print(f"FFmpeg stdout: {stdout.decode()}", file=sys.stderr)
                 if stderr:
-                    print(f"FFmpeg stderr: {stderr.decode()}")
+                    print(f"FFmpeg stderr: {stderr.decode()}", file=sys.stderr)
+                if ffmpeg_process.poll() is not None:
+                    print("FFmpeg process has already terminated.", file=sys.stderr)
             else:
                 print("Error: Data is empty, cannot write to FFmpeg.", file=sys.stderr)
         except Exception as e:
